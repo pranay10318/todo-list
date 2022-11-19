367e9bada3817883e69409b364f343972d9a11ec
pranay10318 <109957442+pranay10318@users.noreply.github.com>
2022-11-19 20:18:04 +0530

    LAST commit

--- src/components/TodoForms.js
+++ src/components/TodoForms.js
@@ -1,11 +1,34 @@
-import React ,{ useState } from 'react';
+import React, { useState } from "react";
 
-export default function TodoForms() {
-    const [input, setinput] = useState('');
-
+export default function TodoForms(props) {
+  const [input, setinput] = useState("");
+  //for changes to be reflected in the input field..
+  const handleChange = (e) => {
+    setinput(e.target.value);
+  };
+  //on submit it will call the props addtodo function in todoform..asdf
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addTodo({
+      id: Math.floor(Math.random() * 10000),
+      text: input,
+      isComplete: false,
+    });
+    setinput("");
+  };
   return (
-    <form>
-
+    <form className="todo-form">
+      <input
+        type="text"
+        placeholder="add a todo"
+        onChange={handleChange}
+        className="todo-input"
+        value={input}
+        name="text"
+      />
+      <button type="submit" className="todo-btn" onClick={handleSubmit}>
+        Add todo
+      </button>
     </form>
-  )
+  );
 }
